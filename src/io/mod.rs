e39659a7aba8f98b2399e6cd402117882ef1fc43
ben <benbaldwin000@gmail.com>
2024-08-14 17:50:46 -0700

    nuke

--- src/io/mod.rs
+++
@@ -1,7 +0,0 @@
-mod error;
-mod fs;
-mod io;
-
-pub use error::*;
-pub use fs::*;
-pub use io::*;
--- src/tok/mod.rs
+++
@@ -1,7 +0,0 @@
-mod error;
-mod tokenizer;
-mod tokens;
-
-pub use error::*;
-pub use tokenizer::*;
-pub use tokens::*;
